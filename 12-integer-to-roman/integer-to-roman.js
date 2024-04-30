04ae85908f43176196d299f6ea1636203c66f4f4
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2024-04-30 20:06:29 +0500

    Time: 104 ms (85.75%) | Memory: 55 MB (52.77%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.js
@@ -0,0 +1,41 @@
+const map = new Map([
+    [1, 'I'],
+    [5, 'V'],
+    [10, 'X'],
+    [50, 'L'],
+    [100, 'C'],
+    [500, 'D'],
+    [1000, 'M']
+])
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    let base = 1;
+    const result = [];
+    while(num > 0){
+        let last = num % 10;
+
+        if(last < 4){
+            for(let k = last; k > 0; k--){
+                result.unshift(map.get(base))
+};
+            } else if(last === 4){
+                result.unshift(...[map.get(base), map.get(base * 5)])
+        }else if(last === 5){
+            result.unshift(map.get(base * 5))
+        }else if(last < 9){
+            for(let k = last; k > 5; k--){
+                result.unshift(map.get(base))
+            } 
+            result.unshift(map.get(base * 5))
+        }else{
+            result.unshift(...[map.get(base), map.get(base * 10)])
+        };
+        base *= 10;
+        num = (num - last) / 10;
+    };
+    return result.join('');
+};
